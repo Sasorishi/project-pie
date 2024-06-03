96e41db6569d40833c8cf09ee8c67b24264b4166
Sasorishi <on_alban@yahoo.fr>
2024-06-03 11:44:11 +0200

    Init project

---
+++ src/app/page.tsx
@@ -0,0 +1,18 @@
+import ECommerce from "@/components/Dashboard/Board";
+import { Metadata } from "next";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+
+export const metadata: Metadata = {
+  title: "Project PIE Dashboard",
+  description: "This is Next.js with TailAdmin Dashboard Template",
+};
+
+export default function Home() {
+  return (
+    <>
+      <DefaultLayout>
+        <ECommerce />
+      </DefaultLayout>
+    </>
+  );
+}
