38cf3844b299dade728756dffcfb9d880f864f12
Sasorishi <on_alban@yahoo.fr>
2024-06-26 14:01:12 +0200

    Fix props children

--- src/components/Hoc/ProtectedRoute.tsx
+++ src/components/Hoc/ProtectedRoute.tsx
@@ -1,27 +1,31 @@
 "use client";
 
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { useAuth } from '@/hooks/useAuth';
+import React, { useEffect, ReactNode } from "react";
+import { useRouter } from "next/navigation";
+import { useAuth } from "@/hooks/useAuth";
 
-const ProtectedRoute = ({ children }) => {
-    const { user, loading } = useAuth();
-    const router = useRouter();
+interface ProtectedRouteProps {
+  children: ReactNode;
+}
 
-    useEffect(() => {
-        if (!loading) {
-            if (!user) {
-                console.log("User is not authenticated, redirecting...");
-                router.push('/auth/signin');
-            }
-        }
-    }, [user, loading, router]);
+const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
+  const { user, loading } = useAuth();
+  const router = useRouter();
 
-    if (loading) {
-        return <div>Loading...</div>;
+  useEffect(() => {
+    if (!loading) {
+      if (!user) {
+        console.log("User is not authenticated, redirecting...");
+        router.push("/auth/signin");
+      }
     }
+  }, [user, loading, router]);
 
-    return children;
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  return children;
 };
 
 export default ProtectedRoute;
--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,37 +1,50 @@
 "use client";
 
-import { useState, useEffect, createContext, useContext } from "react";
+import {
+  useState,
+  useEffect,
+  createContext,
+  useContext,
+  ReactNode,
+} from "react";
 import { onAuthStateChanged, User } from "firebase/auth";
 import { auth } from "@/firebase/firebaseConfig";
 
 interface AuthContextType {
-    user: User | null;
-    loading: boolean;
+  user: User | null;
+  loading: boolean;
 }
 
-const AuthContext = createContext<AuthContextType>({ user: null, loading: true });
+const AuthContext = createContext<AuthContextType>({
+  user: null,
+  loading: true,
+});
 
-export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState<User | null>(null);
-    const [loading, setLoading] = useState(true);
+interface AuthProviderProps {
+  children: ReactNode;
+}
 
-    useEffect(() => {
-        const unsubscribe = onAuthStateChanged(auth, (user) => {
-            console.log("Auth state changed:", user); // Ajout de console log
-            setUser(user);
-            setLoading(false);
-        });
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [user, setUser] = useState<User | null>(null);
+  const [loading, setLoading] = useState(true);
 
-        return () => unsubscribe();
-    }, []);
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      console.log("Auth state changed:", user); // Ajout de console log
+      setUser(user);
+      setLoading(false);
+    });
 
-    return (
-        <AuthContext.Provider value={{ user, loading }}>
-            {children}
-        </AuthContext.Provider>
-    );
+    return () => unsubscribe();
+  }, []);
+
+  return (
+    <AuthContext.Provider value={{ user, loading }}>
+      {children}
+    </AuthContext.Provider>
+  );
 };
 
 export const useAuth = () => {
-    return useContext(AuthContext);
+  return useContext(AuthContext);
 };
